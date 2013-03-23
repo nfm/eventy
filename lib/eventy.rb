71b133173c403ebac2a9d2d86f166e4b035fc871
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2013-03-24 08:47:56 +1100

    Define configure and record methods for Eventy

--- lib/eventy.rb
+++ lib/eventy.rb
@@ -1,5 +1,19 @@
 require "eventy/version"
+require "eventy/abstract_event"
 
 module Eventy
-  # Your code goes here...
+  def self.configure(options = {})
+    case options[:backend].to_sym
+    when :active_record
+      require "eventy/backends/active_record"
+    end
+  end
+
+  def self.record(event_name, identity, properties = {})
+    begin
+      Event.record(event_name, identity, properties)
+    rescue NameError
+      raise "You must call Eventy.configure with a :backend option before calling Eventy.record"
+    end
+  end
 end
